a040826e2bf1061dac33f27e1ed37b17fc24953a
unknown <u77709633@gmail.com>
2025-10-19 22:54:31 +0530

    map changes

--- src/pages/AdminAuth.tsx
+++ src/pages/AdminAuth.tsx
@@ -95,9 +95,11 @@ const AdminAuth = () => {
         <CardContent>
           <Tabs defaultValue="signin" className="w-full">
             {/* <TabsList className="grid w-full grid-cols-2 mb-6">
+            {/* <TabsList className="grid w-full grid-cols-2 mb-6">
               <TabsTrigger value="signin">Sign In</TabsTrigger>
               <TabsTrigger value="signup">Sign Up</TabsTrigger>
             </TabsList> */}
+            
 
             <TabsContent value="signin">
               <form onSubmit={handleSignIn} className="space-y-4">
